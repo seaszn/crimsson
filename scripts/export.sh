93e92b5843731684b186cc6416af8e5a4f3c33c7
Seaszn <111234706+seaszn@users.noreply.github.com>
2025-05-24 15:17:32 +0200

    export fixes

--- scripts/export.sh
+++ scripts/export.sh
@@ -12,4 +12,3 @@ export $(grep -v '^#' .env | xargs)
 
 docker exec n8n-web n8n export:credentials --all --output=services/credentials.json
 docker exec n8n-web n8n export:workflow --all --output=services/workflows.json
-
